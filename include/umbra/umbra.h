745a0f99aa200de17e844eb35592c9599da77f06
binboukami <luiz.butkeivicz@gmail.com>
2024-04-29 19:48:12 -0300

    add export umath api to umbra.h

--- include/umbra/umbra.h
+++ include/umbra/umbra.h
@@ -2,6 +2,7 @@
 
 /* Macro definitions */
 #include "export_macros.h"
+#include "umath.h"
 #include "umbragl.h"
 
 /* Structure defitions */
@@ -20,7 +21,6 @@ extern "C" {
 #endif
 
 /* Function definitions */
-
 /* Window API */
 
 UMBRA_API UWindow* U_InitWindow(const char* title, int width, int height);
@@ -28,6 +28,10 @@ UMBRA_API GLFWwindow* U_GetGLFWHandler();
 UMBRA_API bool U_ShouldCloseWindow(const UWindow* window_ptr);
 UMBRA_API void U_PollWindowEvents();
 UMBRA_API void U_CloseWindow(UWindow* window_ptr);
+
+/* Umbra Math */
+UMBRA_API UMat4x4 U_Mat4x4(); // redefine export
+UMBRA_API UMat4x4 U_Translate(UMat4x4 mat, UVec3 vec3); // redefine export
 
 #ifdef __cplusplus
 }
