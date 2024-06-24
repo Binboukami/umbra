14c315cd173336d0c23d42c0cbc6a72dc5906a40
Binboukami <luiz.butkeivicz@gmail.com>
2024-06-24 07:27:43 -0300

    add more umath definitions to umbra.h

--- include/umbra/umbra.h
+++ include/umbra/umbra.h
@@ -32,6 +32,9 @@ UMBRA_API void U_CloseWindow(UWindow* window_ptr);
 /* Umbra Math */
 UMBRA_API UMat4x4 U_Mat4x4(const f32 scalar); // redefine export
 UMBRA_API UMat4x4 U_Translate(const UMat4x4 mat, const UVec3 vec3); // redefine export
+UMBRA_API UMat4x4 U_Scale(const UMat4x4 mat, const UVec3 vec3);
+UMBRA_API UMat4x4 U_MatMult(const UMat4x4 matA, const UMat4x4 matB);
+UMBRA_API UMat4x4 U_MatOrtho(f32 left, f32 right, f32 bottom, f32 top, f32 near, f32 far);
 
 #ifdef __cplusplus
 }
