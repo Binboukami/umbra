dd17708388eeb8f5af5c8821a9ff7b71e2d223b0
Binboukami <luiz.butkeivicz@gmail.com>
2024-10-10 00:42:58 -0300

    fix typo for vertex attribute stride

--- src/urenderer.c
+++ src/urenderer.c
@@ -1,4 +1,5 @@
 #include <file_system.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include "umbra.h"
@@ -33,7 +34,7 @@ void U_InitRenderer(URenderer* renderer, ui8 use_ebo)
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(renderer->index_buffer), &renderer->index_buffer, GL_DYNAMIC_DRAW);
 
   // Attrib 0: { x, y, z }
-  U_SetVertexAttribute(U_DEFAULT_VERTEX_ATTR_POSITION_IDX, 3, GL_FLOAT, GL_FALSE, sizeof(UVertex), (void*)0);
+  U_SetVertexAttribute(U_DEFAULT_VERTEX_ATTR_POSITION_IDX, 3, GL_FLOAT, GL_FALSE, sizeof(UVertex), 0);
 
   // Attrib 1: { r, g, b }
   U_SetVertexAttribute(U_DEFAULT_VERTEX_ATTR_COLOR_IDX, 3, GL_FLOAT, GL_FALSE, sizeof(UVertex), sizeof(UVec3));
@@ -405,7 +406,7 @@ i32 U_LoadShader(const char* filepath, U_SHADER_TYPE shader_type)
 		return -3;
   }
 
-  size_t new_len = fread(shad_src, sizeof(char), buff_size, file_ptr);
+  fread(shad_src, sizeof(char), buff_size, file_ptr);
 
   if(ferror(file_ptr) != 0)
   {
